63dbb5e986afb07b7bac623834a2e0d5cc839aee
“Shyam <shyamsankar636@gmail.com>
2022-11-15 22:56:55 +0530

    # 1

---
+++ 1 . Band Name Generator.py
@@ -0,0 +1,7 @@
+print ("Welcome to Band Name Generator")
+
+name = input ("Please enter the name of you city :").title()
+
+pet = input ("Please enter the name of your pet :").title()
+
+print (f"your Band name will be : {name} {pet}")
